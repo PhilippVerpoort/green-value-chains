e42693f2b2a0ee49b16d141b59d507acc34af495
Philipp Verpoort <philippv@pik-potsdam.de>
2022-08-04 13:55:55 +0200

    Split data file up into CSV and YML files.

--- src/load/load_default_data.py
+++ src/load/load_default_data.py
@@ -8,39 +8,7 @@ from src.load.yaml_load import loadYamlFile
 default_options = loadYamlFile('data/options.yml')
 units = loadYamlFile('data/units.yml')
 
-fname = getFilePathInput('./data/data.xlsx')
-default_techdata = pd.read_excel(fname, sheet_name='technology_data')
-default_prices = pd.read_excel(fname, sheet_name='price_assumptions')
-process_data = pd.read_excel(fname, sheet_name='processes').set_index('id').to_dict('index')
-
-process_routes_load = pd.read_excel(fname, sheet_name='routes').set_index(['process_group', 'id']).to_dict('index')
-
-for route in process_routes_load.values():
-    tmp = {}
-    for p in route['processes'].split(','):
-        words = p.strip().split('-')
-        if len(words) == 1:
-            tmp[p.strip()] = {}
-        elif len(words) == 2:
-            tmp[words[0]] = {'mode': words[1]}
-        else:
-            raise Exception('Formatting error of processes in route.')
-    route['processes'] = tmp
-
-    if route['import_cases'] and isinstance(route['import_cases'], str):
-        tmp = {}
-        for c in route['import_cases'].split(','):
-            words = c.strip().split(':')
-            tmp[words[0].strip()] = words[1].strip().split('+')
-        route['import_cases'] = tmp
-    else:
-        route['import_cases'] = False
-
-process_routes = {}
-for process_group, route_id in process_routes_load:
-    value = process_routes_load[(process_group, route_id)]
-    value['process_group'] = process_group
-    if process_group not in process_routes:
-        process_routes[process_group] = {route_id: value}
-    else:
-        process_routes[process_group][route_id] = value
+default_techdata = pd.read_csv(getFilePathInput('./data/technologies.csv')).astype({'val': 'float32', 'val_year': 'int32'}, errors='ignore')
+default_prices = pd.read_csv(getFilePathInput('./data/prices.csv')).astype({'val': 'float32', 'val_year': 'int32'}, errors='ignore')
+process_data = pd.read_csv(getFilePathInput('./data/processes.csv')).set_index('id').to_dict('index')
+process_routes = loadYamlFile('data/routes.yml')
